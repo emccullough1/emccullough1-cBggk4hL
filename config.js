cef19dc9d7189d2e4e7eea37784096e6bb3a7734
emccullough1 <emccullough1@eagles.ewu.edu>
2019-06-03 10:04:40 -0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,4 @@
+const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/7ef91d51-aca4-4bb7-a504-8d2432ee6bef/token";
+const instanceLocator = "v1:us1:7ef91d51-aca4-4bb7-a504-8d2432ee6bef";
+
+export { tokenUrl, instanceLocator }
